3f576e3c0de3fbf25b3d303bc74c95f0f2918db5
Jared D. Baker <jbaksta@gmail.com>
2021-10-23 09:49:10 -0600

    small additions to function

--- monolithic/jupyterhub_config.py
+++ monolithic/jupyterhub_config.py
@@ -24,18 +24,18 @@ c.Spawner.http_timeout = 600
 c.Spawner.start_timeout = 600
 c.Spawner.poll_interval = 15
 
-c.JupyterHub.ip = '192.168.99.1'
+c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.port = 8800
 
-api_addr = '192.168.99.1'
+api_addr = '0.0.0.0'
 api_port = 8801
 c.ConfigurableHTTPProxy.api_url = f'http://{api_addr}:{api_port}'
 
 
 base_url = u'/'
 
-c.JupyterHub.default_url = f'{base_url}/user/' + '{username}/lab'
-c.JupyterHub.notebook_dir = u'/home/{username}'
+#c.JupyterHub.default_url = f'{base_url}/user/' + '{username}/lab'
+#c.JupyterHub.notebook_dir = u'/home/{username}'
 
-c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
+#c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
 
